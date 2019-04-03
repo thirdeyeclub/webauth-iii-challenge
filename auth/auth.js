7209a4acb4f8aebebd37407af0fe2d60c9e3ddb5
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-04-03 18:55:25 -0400

    wrote auth router

---
+++ auth/auth.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const secret = require('../secrets').jwtSecret;
+const u = require('../users/users-model');
+
+router.post('/register', (req, res) => {
+let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+user.password = hash;
+
+u.add(user)
+    .then(saved => {res.status(201).json(saved);})
+    .catch(error => {res.status(500).json(error);});
+});
+
+router.post('/login', (req, res) => {
+let { username, password } = req.body;
+u.findBy({ username })
+    .first()
+    .then(user => {
+    if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({message: `Welcome ${user.username}!`,token,});
+    } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+    }
+    })
+    .catch(error => {res.status(500).json(error);});
+});
+
+function generateToken(user) {
+const payload = {
+    subject: user.id,
+    username: user.username,
+    roles: ['student', 'ta'],
+};
+const options = {
+    expiresIn: '1d',
+};
+return jwt.sign(payload, secret, options); 
+}
+
+module.exports = router;
