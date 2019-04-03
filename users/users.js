987cdf6756eec07bf0e1f2456470115c2d095840
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-04-03 19:03:16 -0400

    MVP done

---
+++ users/users.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const u = require('./users-model');
+
+router.get('/', withRole('staff'), (req, res) => {
+u.find()
+    .then(users => {
+    res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+function withRole(role) {
+return function(req, res, next) {
+    if (req.decodedJwt && req.decodedJwt.roles && req.decodedJwt.roles.includes(role)) {
+    next();
+    } else {
+    res.status(403).json({ message: 'FOREBIDDEN' });
+    }
+};
+}
+
+module.exports = router;
